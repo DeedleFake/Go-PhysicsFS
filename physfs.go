b8c16e0fce734543c2b6fd677e9a1a918037ae8b
DeedleFake <yisszev@beckforce.com>
2011-02-14 10:27:23 -0500

    Added version information and some types.

--- physfs.go
+++ physfs.go
@@ -2,6 +2,7 @@ package physfs
 
 import(
 	"os"
+	"unsafe"
 )
 
 // #include <physfs.h>
@@ -11,6 +12,39 @@ func init() {
 	if C.PHYSFS_init(C.CString(os.Args[0])) == 0 {
 		panic(C.PHYSFS_getLastError())
 	}
+}
+
+type File C.PHYSFS_File
+
+type ArchiveInfo struct {
+	Extension string
+	Description string
+	Author string
+	URL string
+}
+
+type Version struct {
+	Major uint8
+	Minor uint8
+	Patch uint8
+}
+
+func GetVersion() (ver *Version) {
+	ver = new(Version)
+
+	ver.Major = C.PHYSFS_VER_MAJOR
+	ver.Minor = C.PHYSFS_VER_MINOR
+	ver.Patch = C.PHYSFS_VER_PATCH
+
+	return ver
+}
+
+func GetLinkedVersion() (ver *Version) {
+	var v C.PHYSFS_Version
+	C.PHYSFS_getLinkedVersion(&v)
+	ver = (*Version)(unsafe.Pointer(&v))
+
+	return ver
 }
 
 func GetBaseDir() (string) {
--- test/test.go
+++ test/test.go
@@ -6,6 +6,11 @@ import(
 )
 
 func main() {
+	ver := physfs.GetVersion()
+	fmt.Printf("Version:\n\tMajor: %v\n\tMinor: %v\n\tPatch: %v\n\n", ver.Major, ver.Minor, ver.Patch)
+	linkver := physfs.GetLinkedVersion()
+	fmt.Printf("LinkedVersion:\n\tMajor: %v\n\tMinor: %v\n\tPatch: %v\n\n", linkver.Major, linkver.Minor, linkver.Patch)
+
 	fmt.Printf("BaseDir: %v\n", physfs.GetBaseDir())
 	fmt.Printf("UserDir: %v\n", physfs.GetUserDir())
 }
